53edf1214cf9d1508de487e479e9dd79e1d367c1
masumvai13548-a11y <masumvai13548@gmail.com>
2026-09-23 13:06:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from telethon import TelegramClient, events, Button
-from telethon.tl.types import KeyboardButtonCallback
 import requests, random, datetime, json, os, re, asyncio, time
 import string
 import hashlib
